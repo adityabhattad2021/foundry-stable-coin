885e2b0020430554ec1e8336c4c4f64a8f8356a0
Aditya Bhattad <adityabhattad18@gmail.com>
2024-05-22 17:01:51 +0530

    chore: add deploy script for DSC

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -2,10 +2,26 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract DeployDSC is Script {
-    
+    address [] public tokenAddresses;
+    address [] public priceFeedAddresses;
+
+    function run() external returns(DecentralizedStableCoin,DSCEngine,HelperConfig){
+        HelperConfig helperConfig = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey ) = helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine dscEngine = new DSCEngine(address(dsc),tokenAddresses, priceFeedAddresses);
+        dsc.transferOwnership(address(dscEngine));
+        vm.stopBroadcast();
+    }
 }
