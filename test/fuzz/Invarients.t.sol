e01713a425f5ebac538af7ce9734f2741c3b0dc2
Aditya Bhattad <adityabhattad18@gmail.com>
2024-05-23 13:24:20 +0530

    setup invarient test

---
+++ test/fuzz/Invarients.t.sol
@@ -0,0 +1,33 @@
+// What are invarients for this smart contracts (Properties that always need to be true).
+
+// 1. Total supply of the DSC should always be less than total value of the collateral deposited
+// 2. Getter view functions should never revert.
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract InvarientsTest is StdInvariant, Test{
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig helperConfig;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, helperConfig) = deployer.run();
+        (, , weth, wbtc, ) = helperConfig.activeNetworkConfig();
+        targetContract(address(dsce));
+    }
+}
